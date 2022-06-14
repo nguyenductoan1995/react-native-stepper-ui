8c6e9f2c18e17dd6a46d8b9f3ec6920a65038d5b
nguyenductoan <nguyenductoann@gmail.com>
2022-06-14 09:39:40 +0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -42,7 +42,7 @@ const Stepper: FC<StepperProps> = (props) => {
     showButton = true,
   } = props;
   const [step, setStep] = useState<number[]>([0]);
-  const listRef = useRef<null>()
+  const listRef = useRef<any>()
   const pushData = (val: number) => {
     setStep((prev) => [...prev, val]);
   };
